e45c6838b718ce8fb3be57310458ad82432f22ed
Gerson Calvo <gedacaba@gmail.com>
2022-07-15 17:05:28 -0500

    comentarios

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,48 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-    contador: 150
-  },
-  mutations: {
-    incrementar(state, payload) {
-      state.contador = state.contador + payload
+    state: {
+        contador: 150
     },
-    disminuir(state, payload) {
-      state.contador = state.contador - payload
-    }
-  },
-  actions: {
-    accionIncrementar({ commit }) {
-      commit('incrementar', 10)
+    mutations: {
+        incrementar(state, payload) {
+            state.contador = state.contador + payload
+        },
+        disminuir(state, payload) {
+            state.contador = state.contador - payload
+        }
     },
-    accionDisminuir({ commit }, numero) {
-      commit('disminuir', numero)
+    actions: {
+        accionIncrementar({ commit }) {
+            commit('incrementar', 10)
+        },
+        accionDisminuir({ commit }, numero) {
+            commit('disminuir', numero)
+        },
+        accionBoton({ commit }, objeto) {
+            if (objeto.estado) {
+                commit('incrementar', objeto.numero)
+            } else {
+                commit('disminuir', objeto.numero)
+            }
+        }
     },
-    accionBoton({ commit }, objeto) {
-      if (objeto.estado) {
-        commit('incrementar', objeto.numero)
-      } else {
-        commit('disminuir', objeto.numero)
-      }
-    }
-  },
-  modules: {
-  }
+    modules: {}
 })
+
+/* notas
+
+las mutaciones acceden a los estados y la pueden modificar y para llamar a esas mutaciones se utulizan las acciones
+
+-Los state siempre vas en las propiedades computadas
+
+-las mutaciones van en los mètodos
+
+-todas las acciones se mapean dentro de los mètodos
+
+
+
+ 
+ 
+ 
+*/
